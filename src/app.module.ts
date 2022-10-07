802ee7dd3047f9e96295b43249fdf2fd1cc65a16
Vladislavpron0 <vladislavpron0@gmail.com>
2022-10-07 16:49:22 +0300

    init models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FilmsController } from './films/films.controller';
+import { FilmsService } from './films/films.service';
+import { FilmsModule } from './films/films.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, FilmsModule, AuthModule],
+  controllers: [AppController, FilmsController],
+  providers: [AppService, FilmsService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Films API')
+    .setDescription('The films API description')
+    .setVersion('1.0')
+    .addTag('films')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.setGlobalPrefix('/api');
   await app.listen(3000);
 }
 bootstrap();
