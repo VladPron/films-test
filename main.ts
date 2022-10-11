d4bccbb21a6238bdb2c07f6cd51dd37fec46e13a
Vladislavpron0 <vladislavpron0@gmail.com>
2022-10-11 10:48:13 +0300

    add procfile

---
+++ main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './src/app.module';
+
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+      .setTitle('Films API')
+      .setDescription('The films API description')
+      .setVersion('1.0')
+      .addTag('films')
+      .build();
+  
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    app.setGlobalPrefix('/api');
+    await app.listen(3000);
+  }
+  bootstrap();
